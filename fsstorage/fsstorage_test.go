ccd3f3b60924e40406374e6b5b59e16175b5eed0
Roy O'Young <roy2220@outlook.com>
2021-02-03 21:32:29 +0800

    Upgrade versionedkv

--- fsstorage/fsstorage_test.go
+++ fsstorage/fsstorage_test.go
@@ -1,32 +1,17 @@
 package fsstorage_test
 
 import (
-	"context"
 	"io/ioutil"
 	"testing"
-	"time"
 
 	"github.com/go-tk/versionedkv"
 	. "github.com/go-tk/versionedkv-fs/fsstorage"
-	"github.com/stretchr/testify/assert"
 )
 
 func TestFSStorage(t *testing.T) {
 	versionedkv.DoTestStorage(t, func() (versionedkv.Storage, error) {
 		return makeStorage()
 	})
-}
-
-func TestRedisStorage_Close(t *testing.T) {
-	s, err := makeStorage()
-	if !assert.NoError(t, err) {
-		t.FailNow()
-	}
-	time.AfterFunc(1*time.Second, func() {
-		s.Close() // WaitForValue should fail with error ErrStorageClosed
-	})
-	_, _, err = s.WaitForValue(context.Background(), "foo", nil)
-	assert.Equal(t, err, versionedkv.ErrStorageClosed)
 }
 
 func makeStorage() (versionedkv.Storage, error) {
--- go.mod
+++ go.mod
@@ -5,7 +5,7 @@ go 1.14
 require (
 	github.com/fsnotify/fsnotify v1.4.9
 	github.com/go-tk/testcase v0.3.0
-	github.com/go-tk/versionedkv v0.2.4
+	github.com/go-tk/versionedkv v0.2.5
 	github.com/rogpeppe/go-internal v1.7.0
 	github.com/rs/xid v1.2.1
 	github.com/stretchr/testify v1.7.0
